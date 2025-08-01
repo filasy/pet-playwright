f66a302e20e6e25a27b4cdc510ab630c5998e3c2
Yuliya Pirogova <yuliya.filatova@sphaera.ru>
2025-08-01 15:36:09 +0300

    feat: Добавлена настройка замедления шагов теста

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,12 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        // launchOptions: {
+        //  slowMo: 500,
+        // },
+      },
     },
 
     // {
