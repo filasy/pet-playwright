f15dad5a77f2c16a9dfcfe34b9a87063db2907c9
Yuliya Pirogova <yuliya.filatova@sphaera.ru>
2025-08-14 09:39:58 +0300

    test: Добавлены тесты Hello

--- tests/API/simple.spec.ts
+++ tests/API/simple.spec.ts
@@ -2,9 +2,19 @@ import { test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { Api } from '../../learnqa-api/Api';
 
-test('[GET] Проверить Hello', async ({ request }) => {
+test('[GET] Проверить Hello c name', async ({ request }) => {
   const myName = faker.person.firstName();
   const response = await new Api(request).hello.get({ name: myName });
   await response.statusCode.shouldBe('OK');
   await response.shouldBe({ answer: `Hello, ${myName}` });
+});
+test('[GET] Проверить Hello без name', async ({ request }) => {
+  const response = await new Api(request).hello.get();
+  await response.statusCode.shouldBe('OK');
+  await response.shouldBe({ answer: `Hello, someone` });
+});
+test('[GET] Проверить Hello c пустым name', async ({ request }) => {
+  const response = await new Api(request).hello.get({ name: '' });
+  await response.statusCode.shouldBe('OK');
+  await response.shouldBe({ answer: `Hello, someone` });
 });
