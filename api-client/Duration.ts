079fcbabf2340ea9ad98f44b2772bb22fc05f977
filasy <jfilasy@gmail.com>
2025-08-19 17:57:12 +0300

    fix: Увеличен responseTime

--- api-client/Duration.ts
+++ api-client/Duration.ts
@@ -3,7 +3,7 @@ import test, { expect } from '@playwright/test';
 export class Duration {
   constructor(private duration: number = 0) {}
   public async shouldBeValid(maxAllowedTime?: number) {
-    const _maxAllowedTime = maxAllowedTime ?? 1000;
+    const _maxAllowedTime = maxAllowedTime ?? 2000;
     return test.step(`Проверка: время выполнения запроса <= ${_maxAllowedTime}ms`, async () => {
       expect(this.duration, `Actual: ${this.duration}ms`).toBeLessThanOrEqual(_maxAllowedTime);
     });
